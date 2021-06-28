24ed5cfe6719aed7ed8019f5bdb0d4bfba4a7321
Giulio Carota <s244930@studenti.polito.it>
2021-06-28 22:55:43 +0200

    set right db

--- server/lib/db.js
+++ server/lib/db.js
@@ -2,7 +2,7 @@ const sqlite3 = require('sqlite3').verbose();
 
 //create a db object;
 
-let db = new sqlite3.Database('./survey_dk.db', (err) => {
+let db = new sqlite3.Database('./survey.db', (err) => {
     if(err) {
         return console.error(err.message);
     }
